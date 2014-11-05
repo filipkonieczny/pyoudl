4e6a809435e7421fff54733b428d4bf595f5eaf1
jhgrng <jhgrng@gmail.com>
2014-11-05 20:37:25 +0100

    Pyoudl script skeleton

---
+++ pyoudl.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+# TODO: documentation
+# description
+
+
+# imports
+import requests
+
+
+# functions
+def main():
+    # TODO: documentation
+    '''
+    '''
+
+    pass
+
+
+# run main function
+if __name__ == "__main__":
+    main()
