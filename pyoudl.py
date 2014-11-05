e7fcbc4088d8cb77ee2fe93fddb552a08675fdcb
jhgrng <jhgrng@gmail.com>
2014-11-05 21:02:34 +0100

    Basic functionality skeleton

--- pyoudl.py
+++ pyoudl.py
@@ -8,10 +8,27 @@
 
 # imports
 import requests
+import sys
+
+
+# TODO: format messages
+# constant variables
+HELLO = '\nHello!\n\n'
+GOODBYE = '\n\nGoodbye!\n'
+NO_LINK_SUPPLIED = 'No link supplied!'
+INVALID_LINK = 'Invalid link!'
 
 
 # functions
-def main():
+def link_validation(link):
+    # TODO: documentation
+    '''
+    '''
+
+    return True
+
+
+def get_single_song():
     # TODO: documentation
     '''
     '''
@@ -19,6 +36,51 @@ def main():
     pass
 
 
+def get_multiple_songs():
+    # TODO: documentation
+    '''
+    '''
+
+    pass
+
+
+def get_music(link):
+    # TODO: documentation
+    '''
+    '''
+
+    pass
+
+
+def main(system_arguments):
+    # TODO: documentation
+    '''
+    '''
+
+    system_arguments_length = len(system_arguments)
+
+    print HELLO
+
+    # check if any system arguments were supplied
+    if system_arguments_length == 1:
+        print NO_LINK_SUPPLIED
+        return
+
+    # iterate through every system argument
+    for i in xrange(1, system_arguments_length):
+        link = system_arguments[i]
+
+        # check if system argument is a valid link
+        if link_validation(link):
+            print 'Link valid! {}'.format(link)
+            get_music(link)
+        else:
+            print INVALID_LINK
+            return
+
+    print GOODBYE
+
+
 # run main function
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
