74b942fc0562c9d3cc0e2c078346c134894fc634
jhgrng <jhgrng@gmail.com>
2014-11-16 20:53:52 +0100

    System arguments input distinction, WIP

--- pyoudl.py
+++ pyoudl.py
@@ -20,13 +20,19 @@ from bs4 import BeautifulSoup
 
 # constant variables
 # TODO: format messages
+BASE_URL = 'http://www.youtube-mp3.org'
+
+PLAYLIST = 'playlist'
+VIDEO = 'video'
+JSON = 'json'
+INVALID = 'invalid'
+
 HELLO = '\nHello!\n\n'
 GOODBYE = '\n\nGoodbye!\n'
 NO_URL_SUPPLIED = 'No url supplied!'
 INVALID_URL = 'Invalid url!'
 DOWNLOADING = "Downloading: '{}'"
 DOWNLOAD_FINISHED = 'Download finished!\n'
-BASE_URL = 'http://www.youtube-mp3.org'
 
 
 # functions
@@ -35,11 +41,52 @@ def url_validation(url):
     '''
     '''
 
+    if not isinstance(url, type('')):
+        return False
+
     if 'http://youtu.be' in url or \
        'https://www.youtube.com' in url:
         return True
 
     return False
+
+
+def get_url_type(url):
+    # TODO: documentation
+    '''
+    '''
+
+    # check if is a youtube link
+    if url_validation(url):
+        # check if is a youtube playlist
+        if 'list=' in url:
+            return PLAYLIST
+
+        # check if is a youtube video
+        if '?v=' in url:
+            return VIDEO
+
+    # TODO: check if is a json file
+    # return JSON
+
+    return INVALID
+
+
+def convert_playlist_to_list():
+    # TODO: documentation
+    '''
+    '''
+
+    pass
+
+
+def convert_json_to_list():
+    # TODO: documentation
+    '''
+    '''
+
+    # TODO: validate every link with url_validation()
+    pass
 
 
 def get_download_data(url):
@@ -94,7 +141,7 @@ def download_song(download_data):
     title = download_data['title']
     download_url = download_data['download_url']
 
-    print DOWNLOADING.format(title)
+    print DOWNLOADING.format(title), 'video.'
 
     # TODO: set '*.mp3' metadata - could be tricky?
     # TODO: check for duplicates
@@ -118,13 +165,15 @@ def get_single_song(url):
     download_song(download_data)
 
 
-def get_multiple_songs():
+def get_multiple_songs(songs_urls):
     # TODO: documentation
     '''
     '''
 
     # TODO: add a counter, like "song 1/12"
-    pass
+
+    for song_url in songs_urls:
+        get_single_song(song_url)
 
 
 def get_music(system_arguments):
@@ -132,16 +181,27 @@ def get_music(system_arguments):
     '''
     '''
 
-    # TODO: determine if it's a playlist
-    # system_arguments_length = len(system_arguments)
-
     # iterate through every system argument
     for url in system_arguments:
         # check if system argument is a valid url
-        if url_validation(url):
-            get_single_song(url)
-        else:
+        url_type = get_url_type(url)
+        if url_type == INVALID:
             print INVALID_URL
+            return
+
+        # if system argument is an url to a video
+        if url_type == VIDEO:
+            get_single_song(url)
+            continue
+
+        # if system argument is an url to a playlist
+        if url_type == PLAYLIST:
+            songs_urls = convert_playlist_to_list()
+        # if system argument is a json file
+        else:
+            songs_urls = convert_json_to_list()
+
+        get_multiple_songs(songs_urls)
 
 
 def main(system_arguments):
